d769751ca7cb6fd4e3d1df97c14259e5b54e18fc
Osmond Oscar <osmond.oranagwa@andela.com>
2016-03-15 15:38:23 +0100

    Implement publishing for twitter

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'koala'
+gem 'twitter'
 gem 'whenever', :require => false
 gem 'figaro'
 
--- app/services/auto_post.rb
+++ app/services/auto_post.rb
@@ -1,10 +1,18 @@
 class AutoPost
   @graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
+  @twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+  end
+
   class << self
 
     def publish(item)
-      profile = @graph.get_object("me")
-      @graph.put_connections("me", "feed", message: "#{item.title} #{item.link}")
+      content = "#{item.title} #{item.link}"
+      @graph.put_connections("me", "feed", message: content)
+      @twitter.update(content)
     end
 
     def publish_updates
